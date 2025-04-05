4f205b7f9d90247c034e84b8b0d179b703a5ee95
CarlosBF <cborrellf@outlook.es>
2025-04-05 19:00:55 +0200

    Background changing function done

---
+++ js/background.js
@@ -0,0 +1,23 @@
+const images = document.getElementById('images')
+let actualImage = 0
+
+
+randomiceBackground()
+
+/* Randomize background without repetition and a blur effect */
+function randomiceBackground() {
+    let imgNumbr = Math.floor(Math.random()*10)+1
+    while(imgNumbr === actualImage){
+        imgNumbr = Math.floor(Math.random()*10)+1
+    }
+    actualImage = imgNumbr
+    /* No need for intervals as css takes care of transitions */
+    images.style.filter = `saturate(0%) blur(100px)`
+    setTimeout(() => {
+        images.style.backgroundImage = `url(../img/${imgNumbr}.jpg)`
+        images.style.filter = `saturate(80%) blur(0px)`
+    }, 500);
+    /* Restart the function with a random timer that ranges between .5 and 1 minute*/
+    let randomTime = Math.floor((Math.random()*10+5)*6000)
+    setTimeout(randomiceBackground, randomTime)
+}
