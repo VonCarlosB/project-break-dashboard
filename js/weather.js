f272098f20dca45d160dc12262facbfaf77f8778
CarlosBF <cborrellf@outlook.es>
2025-04-08 14:24:00 +0200

    Resolved mixed content

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,7 @@ const weatherContainer = document.getElementById('weather')
 
 const getCurrentWeather = async (location) => {
     try{
-        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${APIkey}&q=${location}&aqi=yes`)
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${APIkey}&q=${location}&aqi=yes`)
         if(!response.ok){
             throw new Error('Error en la solicitud', response.status)
         }
@@ -17,7 +17,7 @@ const getCurrentWeather = async (location) => {
 
 const getForecast = async (location) => {
     try{
-        const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${APIkey}&q=${location}&days=1&aqi=no&alerts=no`)
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${APIkey}&q=${location}&days=1&aqi=no&alerts=no`)
         if(!response.ok){
             throw new Error('Error en la solicitud', response.status)
         }
@@ -33,7 +33,7 @@ const currentTemplate = (weather) => {
         <h3>${weather[0]}/${weather[1]}</h3>
         <p>${weather[2]}</p>
         <div class="currentWeather">
-            <img src="${weather[3]}" alt="${weather[2]}">
+            <img src="https:${weather[3]}" alt="${weather[2]}">
             <div>
                 ${weather[4]}
                 <img src="./../img/thermometer.png" alt="celsius" width=25>
@@ -51,7 +51,7 @@ const forecastTemplate = (hour) => {
     return `
         <div>
             <p>${hour.time.slice(10)}</p>
-            <img src="${hour.condition.icon}" alt="${hour.condition.text}">
+            <img src="https:${hour.condition.icon}" alt="${hour.condition.text}">
             <p>${hour.temp_c} ºC</p>
         </div>
     `
